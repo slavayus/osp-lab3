8f37080c62bb2b42071f6bd7fdbfd7257b8dc13a
vlad <slavayus@gmail.com>
2018-11-17 22:37:34 +0300

    fetch other user, who can edit file

--- sc.sh
+++ sc.sh
@@ -5,35 +5,62 @@ if [[ ! -f "$1" ]]; then
 fi
 
 userName=`ls -l "$1" | awk -F" " '{print $3}'`
+users=("${userName}")
 
-if [[ -r "$1" ]]
+if [[ -w "$1" ]]
 then
-        echo $userName
+        echo ${userName}
 fi
 
-lsout=`ls -l "$1"`
-groupw=${lsout:5:1}
-if [[ $groupw = "w" ]]; then
-    groupId=`ls -ln "$1" | awk -F" " '{print $4}'`;
-fi
-
-passwds=(`getent passwd`)
+groupId=`ls -ln "$1" | awk -F" " '{print $4}'`;
 OLDIFS=$IFS
 IFS=","
-groups=(`getent group $groupId | awk -F":" '{print $4}'`)
-IFS=$OLDIFS
-for passwd in "${passwds[@]}"
+groups=(`getent group ${groupId} | awk -F":" '{print $4}'`)
+for group in "${groups[@]}"
 do
-    passwdName=`echo $passwd | awk -F":" '{print $1}'`
-    passwdGId=`echo $passwd | awk -F":" '{print $4}'`;
-    if [[ $passwdGId = $groupId ]]; then
-        echo $passwdName
-    else
-      for group in "${groups[@]}"
+    users=("${users[@]}" "${group}")
+done
+IFS=$'\n'
+passwds=(`getent passwd`)
+IFS=${OLDIFS}
+
+lsout=`ls -l "$1"`
+groupW=${lsout:5:1}
+if [[ ${groupW} = "w" ]]; then
+    for passwd in "${passwds[@]}"
+    do
+        passwdName=`echo ${passwd} | awk -F":" '{print $1}'`
+        passwdGId=`echo ${passwd} | awk -F":" '{print $4}'`
+        if [[ ${passwdGId} = ${groupId} && ${passwdName} != ${userName} ]]; then
+            echo ${passwdName}
+        else
+          for group in "${groups[@]}"
+            do
+                if [[ ${group} = ${passwdName} && ${group} != ${userName} ]]; then
+                    echo ${group}
+                fi
+            done
+        fi
+    done
+fi
+
+otherW=${lsout:8:1}
+if [[ ${otherW} = "w" ]]; then
+    for passwd in "${passwds[@]}"
+    do
+        passwdName=`echo ${passwd} | awk -F":" '{print $1}'`
+        contains="false"
+        for user in "${users[@]}"
         do
-            if [[ $group = $passwdName && $group != $userName ]]; then
-                echo $group
+            echo $user
+            echo $passwdName
+            if [[ ${user} = ${passwdName} ]]; then
+                contains="true"
+                echo "true"
             fi
         done
-    fi
-done
+        if [[ ${contains} = "false" ]] ; then
+            echo ${passwdName}
+        fi
+    done
+fi
